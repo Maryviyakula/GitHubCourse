284c5d26d96c676ea9d99c5fcf481c21561c19e3
Maryviyakula <vmarypeter@gmail.com>
2022-12-04 08:37:16 +0800

    added/updated/deleted files

--- src/Pages/LoginPage.java
+++ src/Pages/LoginPage.java
@@ -3,8 +3,11 @@ package Pages;
 public class LoginPage {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("Hello LoginPage");
 
+		String user="Admin";
+		String pwd="Admin";
+		System.out.println("User & Password"+user+' '+pwd);
 	}
 
 }
